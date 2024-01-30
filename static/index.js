3ccd1c970654569c307ce171bed362daf099db46
Yasmin Hackbart <90115526+yasminhackbart@users.noreply.github.com>
2024-01-30 17:55:16 -0300

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,1 +1,43 @@
 
+var date = new Date()
+let display_date= "Data: " + date.toLocaleDateString('pt-BR', {weekday: 'short', year: 'numeric', month: 'short', day: 'numeric'})
+
+//Carregar o DOM HTML
+$(document).ready(function () {
+    $("#display_date").html(display_date)
+})
+
+let predicted_emotion;
+//HTML-->JavaScript--->Flask
+//Flask--->JavaScript--->HTML
+
+$(function () {
+    $("#predict_button").click(function () {
+
+        let input_data = {
+            "text": $("#text").val()
+        }
+        console.log(input_data)
+
+        $.ajax({
+            type: ,
+            url: ",
+            data: ,
+            dataType:,
+            contentType: ,
+            success: function (result) {
+                
+                // Resultado recebido do Flask ----->JavaScript
+                predicted_emotion = result.data.predicted_emotion
+                emo_url = result.data.predicted_emotion_img_url
+
+                
+                // Exibir resultado usando JavaScript----->HTML
+              
+            },
+            error: function (result) {
+                alert(result.responseJSON.message)
+            }
+        });
+    });
+})
